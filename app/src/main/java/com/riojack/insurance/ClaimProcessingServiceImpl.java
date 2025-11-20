3265a9d2f85f868cbc973f5d5bde912c16322ad7
riojack <riojack@users.noreply.github.com>
2025-11-20 15:15:56 -0600

    green: exception if claim amount less than zero

--- app/src/main/java/com/riojack/insurance/ClaimProcessingServiceImpl.java
+++ app/src/main/java/com/riojack/insurance/ClaimProcessingServiceImpl.java
@@ -1,8 +1,11 @@
 package com.riojack.insurance;
 
+import com.riojack.insurance.exceptions.ClaimValidationException;
 import com.riojack.insurance.pojos.Claim;
 
 public class ClaimProcessingServiceImpl implements ClaimProcessingService {
     @Override
-    public void getClaimPayout(Claim claim) {}
+    public void getClaimPayout(Claim claim) {
+        throw new ClaimValidationException();
+    }
 }
